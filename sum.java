a032be7eb43f861d6b5f019f8bf0b6808d097377
Guddu <sdas60418@gmail.com>
2023-01-07 23:34:51 +0530

    change files

--- sum.java
+++ sum.java
@@ -9,6 +9,6 @@ public static void main(String[] args){
    int number2=scn.nextInt();
     int sum=number1+number2;
     System.out.print(sum);
-
+   System.out.print("what is your name ??");
  }
 }
