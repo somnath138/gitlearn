5adc7eda80ea7cee3d062820581f42bf0bbd18f3
Guddu <sdas60418@gmail.com>
2023-01-07 18:44:35 +0530

    change files

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sum{
+
+public static void main(String[] args){
+
+   Scanner scn=new Scanner(System.in)
+   int number1=scn.nextInt();
+   int number2=scn.nextInt();
+    int sum=number1+number2;
+    System.out.print(sum);
+
+ }
+}
