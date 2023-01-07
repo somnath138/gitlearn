2678ed6958c5516329cc6cbc10d574424efc7064
Guddu <sdas60418@gmail.com>
2023-01-08 03:12:08 +0530

    this file only for addition

--- sum.java
+++ sum.java
@@ -9,7 +9,7 @@ public static void main(String[] args){
    int number2=scn.nextInt();
     int sum=number1+number2;
     System.out.print(sum);
-   System.out.print("what is your name ??");
+   //System.out.print("what is your name ??");
    
  }
 }
